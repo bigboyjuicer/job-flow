6139d2a431e07553011e47aebe7cd824d18f4f64
Maksim Zinin <m.zinin@inbox.ru>
2026-07-07 00:33:53 +0300

    Vacancy service initial commit

---
+++ vacancy-service/src/main/java/ru/rekklez/vacancyservice/VacancyserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.rekklez.vacancyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyserviceApplication.class, args);
+	}
+
+}
---
+++ vacancy-service/src/test/java/ru/rekklez/vacancyservice/VacancyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rekklez.vacancyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
