5ab1b71a2a0844f7e2e947dd53b7342b3eb0c8c1
Maksim Zinin <m.zinin@inbox.ru>
2026-07-11 19:28:20 +0300

    Setted up security configuration

--- vacancy-service/src/main/java/ru/rekklez/vacancyservice/security/SecurityConfig.java
+++ vacancy-service/src/main/java/ru/rekklez/vacancyservice/security/SecurityConfig.java
@@ -3,7 +3,13 @@ package ru.rekklez.vacancyservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import ru.rekklez.JwtAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -12,8 +18,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfiguration()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                //.addFilterAt(new JwtAuthenticationFilter());
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        return request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080"));
+            configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+            configuration.setAllowedHeaders(List.of("Authorization"));
+            return configuration;
+        };
     }
 
 }
